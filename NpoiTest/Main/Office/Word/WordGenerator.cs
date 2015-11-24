f25794f54181b6d03b38941a4c4fa992ba4dc849
ssthouse <1194368995@qq.com>
2015-11-24 15:24:26 +0800

    word导出图片部分大概当成

--- NpoiTest/Main/Office/Word/WordGenerator.cs
+++ NpoiTest/Main/Office/Word/WordGenerator.cs
@@ -22,18 +22,11 @@ namespace NpoiTest.Office.Word
         /// </summary>
         private readonly List<DbBean> dataList;
 
-        //输出路径
-        private readonly string outputPath;
-
         /// <summary>
-        ///     构造方法
+        /// 禁止创建实例
         /// </summary>
-        /// <param name="dataList">需要导出的数据</param>
-        /// <param name="outputPath">输出路径</param>
-        public WordGenerator(List<DbBean> dataList, string outputPath)
+        private WordGenerator()
         {
-            this.dataList = dataList;
-            this.outputPath = outputPath;
         }
 
         /// <summary>
@@ -51,12 +44,13 @@ namespace NpoiTest.Office.Word
                 word_inster_table(m_Docx, datalist[i].DeviceType, datalist[i].KilometerMark,
                     datalist[i].SideDirection, datalist[i].Longitude, datalist[i].Latitude, i + 1);
                 //TODO 此处插入图片
+                word_insert_picture(m_Docx, datalist[i].PrjName, datalist[i].PhotoPathName);
             }
             //输出
             try
             {
-                FileStream sw = File.Create(outputPath); 
-                m_Docx.Write(sw);                           
+                FileStream sw = File.Create(outputPath);
+                m_Docx.Write(sw);
                 sw.Close();
             }
             catch
@@ -69,7 +63,7 @@ namespace NpoiTest.Office.Word
         /// 生成模板文件
         /// </summary>
         /// <param name="m_Docx">根文档</param>
-        static void word_init(XWPFDocument m_Docx)
+        private static void word_init(XWPFDocument m_Docx)
         {
             //1‘=1440twip=25.4mm=72pt(磅point)=96px(像素pixel)
             //1px(像素pixel)=0.75pt(磅point)
@@ -81,10 +75,9 @@ namespace NpoiTest.Office.Word
             //16k197mmX273mm:
             CT_SectPr m_SectPr = new CT_SectPr();
             //页面设置A4纵向
-            m_SectPr.pgSz.w = (ulong)11906;
-            m_SectPr.pgSz.h = (ulong)16838;
+            m_SectPr.pgSz.w = (ulong) 11906;
+            m_SectPr.pgSz.h = (ulong) 16838;
             m_Docx.Document.body.sectPr = m_SectPr;
-
             //第一页
             word_insert_space(4, m_Docx);
             word_insert_text(m_Docx, "宋体", 22, "【项目名称】");
@@ -95,7 +88,6 @@ namespace NpoiTest.Office.Word
             word_insert_text(m_Docx, "宋体", 22, "【日期】");
 
             word_insert_space(7, m_Docx);
-
             //第二页
             //表1
             XWPFTable table = m_Docx.CreateTable(4, 2);
@@ -107,8 +99,7 @@ namespace NpoiTest.Office.Word
             CT_TcPr m_Pr = table.GetRow(0).GetCell(1).GetCTTc().AddNewTcPr();
             m_Pr.tcW = new CT_TblWidth();
             m_Pr.tcW.w = "4000";
-            m_Pr.tcW.type = ST_TblWidth.dxa;//设置单元格宽度
-
+            m_Pr.tcW.type = ST_TblWidth.dxa; //设置单元格宽度
             word_insert_space(2, m_Docx);
             word_insert_text(m_Docx, "宋体", 12, "基站勘察表");
         }
@@ -119,6 +110,7 @@ namespace NpoiTest.Office.Word
          *@m_Docx 根文档
          *@longth 空行高
          */
+
         private static void word_insert_space(int n, XWPFDocument m_Docx, int longth = 250)
         {
             for (int i = 0; i < n; i++)
@@ -130,15 +122,16 @@ namespace NpoiTest.Office.Word
             }
         }
 
-        /*
-         *插入文字函数
-         *@m_Docx根文档
-         *@Font 字体
-         *@size 大小
-         *@text 内容
-         *@position 相对位置
-         */
-        private static void word_insert_text(XWPFDocument m_Docx, string Font, int size, string text, ParagraphAlignment position = ParagraphAlignment.CENTER)
+        /// <summary>
+        /// 插入文字函数
+        /// </summary>
+        /// <param name="m_Docx"></param>
+        /// <param name="Font"></param>
+        /// <param name="size"></param>
+        /// <param name="text"></param>
+        /// <param name="position"></param>
+        private static void word_insert_text(XWPFDocument m_Docx, string Font, int size, string text,
+            ParagraphAlignment position = ParagraphAlignment.CENTER)
         {
             XWPFParagraph gp = m_Docx.CreateParagraph(); //创建XWPFParagraph
             gp.SetAlignment(position);
@@ -157,7 +150,8 @@ namespace NpoiTest.Office.Word
         /// <param name="side_direction">下行侧向</param>
         /// <param name="longitude">经度</param>
         /// <param name="latitude">纬度</param>
-        private static void word_inster_table(XWPFDocument m_Docx, string device_type = "", string kilometer_mark = "", string side_direction = "", string longitude = "", string latitude = "", int i = 1)
+        private static void word_inster_table(XWPFDocument m_Docx, string device_type = "", string kilometer_mark = "",
+            string side_direction = "", string longitude = "", string latitude = "", int i = 1)
         {
             XWPFTable table = m_Docx.CreateTable(12, 2);
             CT_Tbl ctbl = m_Docx.Document.body.GetTblArray()[i];
@@ -186,7 +180,7 @@ namespace NpoiTest.Office.Word
             CT_TcPr m_Pr = table.GetRow(2).GetCell(1).GetCTTc().AddNewTcPr();
             m_Pr.tcW = new CT_TblWidth();
             m_Pr.tcW.w = "3500";
-            m_Pr.tcW.type = ST_TblWidth.dxa;//设置单元格宽度
+            m_Pr.tcW.type = ST_TblWidth.dxa; //设置单元格宽度
 
             XWPFTableRow m_Row = table.InsertNewTableRow(0);
             XWPFTableCell cell = m_Row.CreateCell();
@@ -203,77 +197,23 @@ namespace NpoiTest.Office.Word
             word_insert_space(1, m_Docx, 100);
         }
 
-        static void word_insert_picture(XWPFDocument m_Docx, string prjName, string path)
-        {
-            System.IO.DirectoryInfo dir = new System.IO.DirectoryInfo(".\\Picture\\" + prjName + "\\" + path);
-            System.IO.FileInfo[] files = dir.GetFiles(); // 获取所有文件信息。。
-            foreach (System.IO.FileInfo file in files)
-            {
-                FileStream gfs = new FileStream(".\\Picture\\" + prjName + "\\" + path + "\\" + file.Name, FileMode.Open, FileAccess.Read);
-                XWPFParagraph gp = m_Docx.CreateParagraph(); //创建XWPFParagraph
-               // gp.SetAlignment = ParagraphAlignment.CENTER;
-                XWPFRun gr = gp.CreateRun();
-                gr.AddPicture(gfs, (int)NPOI.XWPF.UserModel.PictureType.JPEG, file.Name, 1000000, 1000000);
-                gfs.Close();
-            }
-        }
-
-        /// <summary>
-        /// 读取db数据库
-        /// </summary>
-        /// <returns>返回读取到的数据列表</returns>
-        static List<List<DbBean>> SQLite_Read()
-        {
-            List<List<DbBean>> datalist = new List<List<DbBean>>();
-            List<DbBean> dtalst = new List<DbBean>();
-            string path = ".\\Picture\\Location.db";
-            SQLiteConnection conn = new SQLiteConnection("Data Source=" + path);
-            conn.Open();
-            string sql = "select * from Markers order by prjName";
-            string prj = "have no prjName";
-            SQLiteCommand command = new SQLiteCommand(sql, conn);
-            SQLiteDataReader reader = command.ExecuteReader();
-            while (reader.Read())
-            {
-                if (prj.Equals(reader["prjName"].ToString()) || prj.Equals("have no prjName"))
-                {
-                    DbBean dbBean = new DbBean();
-                    dbBean.PrjName = reader["prjName"].ToString();
-                    prj = dbBean.PrjName;
-                    dbBean.DeviceType = reader["device_type"].ToString();
-                    dbBean.KilometerMark = reader["kilometer_mark"].ToString();
-                    dbBean.SideDirection = reader["side_direction"].ToString();
-                    dbBean.Longitude = reader["longitude"].ToString();
-                    dbBean.Latitude = reader["latitude"].ToString();
-                    dbBean.PhotoPathName = reader["photoPathName"].ToString();
-                    dtalst.Add(dbBean);
-                }
-                else
-                {
-                    datalist.Add(dtalst);
-                    dtalst = new List<DbBean>();
-                    prj = "have no prjName";
-                }
-            }
-            datalist.Add(dtalst);
-            return datalist;
-        }
-
         /// <summary>
         /// word文档插入图片
         /// </summary>
-        public static void InsertPic()
+        private static void word_insert_picture(XWPFDocument m_Docx, string prjName, string photoPathName)
         {
-            XWPFDocument document = new XWPFDocument();
-            FileStream fileStream = new FileStream(@"F:\test\test.docx", FileMode.OpenOrCreate);
-            FileStream picStraStream = new FileStream(@"F:\test\test.jpg", FileMode.Open);
-            XWPFParagraph paragraph = document.CreateParagraph();
-            paragraph.SetAlignment(ParagraphAlignment.CENTER);
-            XWPFRun run = paragraph.CreateRun();
-            XWPFPicture picture = new XWPFPicture(new NPOI.OpenXmlFormats.Dml.Picture.CT_Picture(), run);
-            run.AddPicture(picStraStream, (int)PictureType.JPEG, "test.jpg", 1000000, 1000000);
-            document.Write(fileStream);
-            fileStream.Close();
+            System.IO.DirectoryInfo dir = new System.IO.DirectoryInfo(".\\Picture\\" + prjName + "\\" + photoPathName);
+            System.IO.FileInfo[] files = dir.GetFiles(); // 获取所有文件信息。。
+            foreach (System.IO.FileInfo file in files)
+            {
+                FileStream gfs = new FileStream(".\\Picture\\" + prjName + "\\" + photoPathName + "\\" + file.Name,
+                    FileMode.Open, FileAccess.Read);
+                XWPFParagraph gp = m_Docx.CreateParagraph(); //创建XWPFParagraph
+                gp.SetAlignment(ParagraphAlignment.CENTER);
+                XWPFRun gr = gp.CreateRun();
+                gr.AddPicture(gfs, (int) NPOI.XWPF.UserModel.PictureType.JPEG, file.Name, 1000000, 1000000);
+                gfs.Close();
+            }
         }
     }
 }
